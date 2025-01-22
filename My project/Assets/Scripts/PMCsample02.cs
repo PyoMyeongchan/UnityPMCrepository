ba2bbe43de6b951b69a3b6a9612747bee6372993
PyoMyeongchan <vyaudcks@gmail.com>
2025-01-22 12:42:14 +0900

    2025-01-22
    mode

--- My project/Assets/Scripts/PMCsample02.cs
+++ My project/Assets/Scripts/PMCsample02.cs
@@ -4,25 +4,19 @@ using UnityEngine;
 public class PMCsample02 : MonoBehaviour
 {
     [Tooltip("체크하면 점프가 가능합니다.")]
-    public bool isdead = true;
+    public bool isjump = true;
 
     [Header("과일바구니 안의 과일")]
-    public string fruit;
+    public string[] basket;
 
     public int money;
 
-    [Range(1,99)]
-    public int fieldview;
+    [Range(1.0f,99)]
+    public float fieldofview;
 
     [Flags]
     public enum RAINBOW
     {
-        빨 = 1 << 0,
-        주 = 1 << 1,
-        노 = 1 << 2,
-        초 = 1 << 3,
-        파 = 1 << 4,
-        남 = 1 << 5,
-        보 = 1 << 6
+        빨,주,노,초,파,남,보
     }
 }
