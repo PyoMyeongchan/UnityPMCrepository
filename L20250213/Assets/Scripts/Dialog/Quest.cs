0980c30b2805c25de5b58ddb21c28e4cbd1f0f91
PyoMyeongchan <vyaudcks@gmail.com>
2025-02-13 17:27:05 +0900

    QuestDialog

--- L20250213/Assets/Scripts/Dialog/Quest.cs
+++ L20250213/Assets/Scripts/Dialog/Quest.cs
@@ -6,29 +6,40 @@ using UnityEngine.UI;
 
 public class Quest : MonoBehaviour
 {
-    public Text talk;
-    public Button next;
-
+    public GameObject dialog;
+    public Text text;
     public Queue<string> stringQueue = new Queue<string>();
-        
-
-    public int Count //큐 길이 가져오기
-    {
-        get { return stringQueue.Count; }
-    }
-
-
+    
     public void Start()
     {
+        
         stringQueue.Enqueue("안녕하신가 자네 시간이 되면 날 도와주게나.");
         stringQueue.Enqueue("무슨 일이시죠?");
         stringQueue.Enqueue("나 배가고파 빵을 사다주게.");
         stringQueue.Enqueue("아..알겠습니다.");
+                      
 
-        foreach (string dialog in stringQueue)
-        {
-            talk.text = stringQueue.Peek();
-        }
     }
 
+    public void NextButtonCLick()
+    {
+        dialog.SetActive(true);
+
+        StartCoroutine("talk");
+    }
+
+
+    IEnumerator talk()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            text.text = stringQueue.Dequeue();
+            yield return new WaitForSeconds(5.0F);
+        }
+        dialog.SetActive(false);
+    }
+
+
 }
+
+    
